7970c2f376a98f458cb36f3a34674beb31a8bd21
brown <mayconbrown083@gmail.com>
2025-10-20 01:51:19 -0300

    Delete sw.js

--- ferramentas-e-utilitarios/pdv/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Apenas um fetch básico para PWA funcionar
-});
